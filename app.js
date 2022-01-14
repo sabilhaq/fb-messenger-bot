07be22a040da052fb777206b4f051992498e15eb
sabilhaq <makrifatsabilhaq@gmail.com>
2022-01-14 11:11:22 +0700

    add https config

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+const fs = require('fs');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var webhookRouter = require('./routes/webhook');
 
 var app = express();
+
+// Certificate
+const privateKey = fs.readFileSync(process.env.PRIVKEY, 'utf8');
+const certificate = fs.readFileSync(process.env.CERT, 'utf8');
+const ca = fs.readFileSync(process.env.CHAIN, 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,4 +32,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/webhook', webhookRouter);
 
-module.exports = app;
+module.exports = {
+  app,
+  credentials,
+};
